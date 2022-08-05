2bd389afba47e875edd3c1f763b49ed5f907dc0d
ismetozkan <ismetozkan32@hotmail.com>
2022-08-05 13:59:13 +0200

    Addititon for expense.amount

--- src/App.svelte
+++ src/App.svelte
@@ -4,13 +4,16 @@
     // Components
 	import Navbar from "./components/Navbar.svelte";
     import ExpensesList from "./components/ExpensesList.svelte";
-
+    import Total from "./components/Total.svelte";
     // Data
     import expensesData from "./expenses.js";
 
     // Variables
     let expenses = [...expensesData];
     console.log(expenses);
+        // sum values instantly
+    $: total = expenses.reduce((preVal, currVal) =>
+            preVal + currVal.amount, 0);
 
     // Functions
     function removeExpense(id){
@@ -19,20 +22,21 @@
 
     function clearExpenses(){
         expenses = [];
+        // todo: ADD PRESS & HOLD TO CLEAR EXPENSE BUTTON
     }
     // Context
     setContext('remove',removeExpense )
-
 </script>
 <Navbar />
 <main class="content">
+    <Total title="Total Expenses" { total }/>
     <ExpensesList {expenses} />
+
     <button type="button" class="btn btn-primary btn-block"
             on:click = {clearExpenses}>
         Clear Expenses
     </button>
 </main>
-
 
 
 
---
+++ src/components/Total.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let title ='Default Title'
+    export let total = 0;
+</script>
+
+<section class="main-title">
+    <h2>{title}: € {total}</h2>
+</section>
